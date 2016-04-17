cca11b1404285cf863f7e5abf598f6ecdbe9c5ee
MarcinWirkus <marcin@inkre.pl>
2016-04-17 16:56:54 +0200

    planing-trip

--- prototypes/dist/js/main.js
+++ prototypes/dist/js/main.js
@@ -647,3 +647,13 @@ var busStop = [
         }
     }
 ];
+
+$(document).ready(function() {
+    $(".date-picker").datepicker();
+
+    $(".date-picker").on("change", function () {
+        var id = $(this).attr("id");
+        var val = $("label[for='" + id + "']").text();
+        $("#msg").text(val + " changed");
+    });
+});
