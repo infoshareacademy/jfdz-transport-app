0e004f2cf74f4be8e6159c90f7907cedb38e4a16
MarcinWirkus <marcin@inkre.pl>
2016-05-07 17:04:09 +0200

    planning trip ordung ;)

---
+++ prototypes/src/js/planning.js
@@ -0,0 +1,39 @@
+var planning = angular.module('planning',[]);
+
+planning.constant('planningAPI', 'http://isa-api.herokuapp.com/transport/lines.json');
+
+angular.module('planning').service('planningService', function($http, planningAPI){
+
+    this.getPlanningFor = function() {
+        var url = planningAPI;
+
+        return $http({
+            method: 'GET',
+            url: url
+        })
+    }
+});
+
+
+angular.module('planning').controller('planningController', function($scope, planningService){
+    $scope.planning = {};
+    $scope.planning.loading = true;
+
+    $scope.success1 = function (response) {
+
+        $scope.planning.loading = false;
+        $scope.planning.cityname = response.data.name;
+
+        console.log(response);
+
+    };
+
+    $scope.error1 = function(response){
+        console.log('error', response);
+        $scope.planning.error = true;
+    };
+
+    planningService.getPlanningFor().then($scope.success1,$scope.error1 );
+
+});
+
