a9d841e5c9b3ab12159004af02e474afabd68538
kamilazabik <kamilazabik@gmail.com>
2016-05-07 21:38:55 +0200

    próby z godzinami

--- prototypes/src/js/main.js
+++ prototypes/src/js/main.js
@@ -7,33 +7,33 @@ var app = angular.module("app", []);
 app.controller('HomeCtrl', function ($scope) {
     console.log("Home Controller!");
     $scope.name = 'Agnieszka C';
-});
-app.controller('ExampleController', ['$scope', '$http', '$log', function($scope, $http, $log) {
-
-    $scope.busesStops = [];
-    $scope.busesLines =[];
-
-    $http.get('http://isa-api.herokuapp.com/transport/stops.json')
-        .success(function (data, status, headers, config) {
-
-            $scope.busesStops = data;
-            $log.info('Działa!');
-
-        })
-        .error(function (data, status, headers, config) {
-            $log.error('Wystąpił błąd '+status+' podczas pobierania listy przystanków!');
-        });
-
-    $http.get('http://isa-api.herokuapp.com/transport/lines.json')
-    .success(function (data, status, headers, config) {
-
-        $scope.busesLines = data;
-        $log.info('Działa 2!');
-
-    })
-        .error(function (data, status, headers, config) {
-            $log.error('Wystąpił błąd '+status+' podczas pobierania listy lini!');
-        });
+})
+//app.controller('ExampleController', ['$scope', '$http', '$log', function($scope, $http, $log) {
+//
+//    $scope.busesStops = [];
+//    $scope.busesLines =[];
+//
+//    $http.get('http://isa-api.herokuapp.com/transport/stops.json')
+//        .success(function (data, status, headers, config) {
+//
+//            $scope.busesStops = data;
+//            $log.info('Działa!');
+//
+//        })
+//        .error(function (data, status, headers, config) {
+//            $log.error('Wystąpił błąd '+status+' podczas pobierania listy przystanków!');
+//        });
+//
+//    $http.get('http://isa-api.herokuapp.com/transport/lines.json')
+//    .success(function (data, status, headers, config) {
+//
+//        $scope.busesLines = data;
+//        $log.info('Działa 2!');
+//
+//    })
+//        .error(function (data, status, headers, config) {
+//            $log.error('Wystąpił błąd '+status+' podczas pobierania listy lini!');
+//        });
 
     //$scope.data =
     //    //{
@@ -55,7 +55,7 @@ app.controller('ExampleController', ['$scope', '$http', '$log', function($scope,
     //            //{id: '301', name: 'Gdańska'}
     //        ]
         //};
-    }]);
+
 
 
 
