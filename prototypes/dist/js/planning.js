b14b83139cfbe64bd340d665c164f19e2e295801
MarcinWirkus <marcin@inkre.pl>
2016-05-08 16:53:50 +0200

    planning trip v.0.0.3

--- prototypes/dist/js/planning.js
+++ prototypes/dist/js/planning.js
@@ -4,14 +4,11 @@ var planning = angular.module('planning', []);
 planning.controller('planningController', function($scope, busService){
 
     $scope.planning = {};
-    $scope.xlines = 0;
     $scope.planning.loading = true;
 
     $scope.success1 = function (response) {
         $scope.planning.loading = false;
 
-
-        $scope.planning = {};
         $scope.planning = response.data;
 
 
@@ -25,12 +22,20 @@ planning.controller('planningController', function($scope, busService){
 
     };
 
+    $scope.stops = function(response) {
+        $scope.planning = response.data;
+
+        console.log('Linia:',$scope.formlines);
+
+    };
+
     $scope.error1 = function(response){
         console.log('error', response);
         $scope.planning.error = true;
     };
 
     busService.getLinesFor().then($scope.success1,$scope.error1 );
+    busService.getStopsFor().then($scope.stops,$scope.error1);
 
 });
 
