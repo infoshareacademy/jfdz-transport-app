f62d414a7c3c77c599e9560fe2c330bed1bf560a
M.Wirkus <marcin@inkre.pl>
2016-05-11 19:26:42 +0200

    planning trip 0.0.3.2

--- prototypes/src/js/planning.js
+++ prototypes/src/js/planning.js
@@ -1,7 +1,6 @@
-
 var planning = angular.module('planning', []);
 
-planning.controller('planningController', function($scope, busService){
+planning.controller('planningController', function ($scope, busService) {
 
     $scope.planning = {};
     $scope.planning.loading = true;
@@ -13,42 +12,64 @@ planning.controller('planningController', function($scope, busService){
 
     $scope.planStops = {};
 
-    $scope.stops = function(resStops) {
+    $scope.stops = function (resStops) {
         $scope.planStops = resStops.data;
 
 
         $scope.$watch('formstops', function (item) {
 
             if (item != undefined) {
-                var id = item;
                 $scope.buses = [];
-                var x = $scope.planStops[id].bus;
+                var x = $scope.planStops[item].bus;
                 x.forEach(function (line) {
                     $scope.buses.push(line);
                 });
+                if (item != undefined) {
+                    $scope.$watch('formlines', function (fitem) {
+                        $scope.lines = [];
+                        var y = $scope.planStops[item].bus[fitem];
+                        console.log($scope.planStops[item].bus[fitem].departures);
+                        y.forEach(function (line) {
+                            $scope.lines.push(line);
+                        });
+                        console.log($scope.lines);
+                    });
+                }
             }
         });
 
-      //  console.log($scope.planStops[0].bus[0]);
 
-        $scope.addLine = function() {
+        $scope.planningStage = [];
+
+        $scope.addLine = function () {
             $scope.day = $scope.formday;
             $scope.stops = $scope.formstops;
             $scope.lines = $scope.formlines;
+            $scope.time = $scope.formhours;
+            $scope.planningStage.push({
+                day: $scope.day,
+                stops: $scope.stops,
+                lines: $scope.lines,
+                time: $scope.time
+            });
+            //console.log($scope.planningStage);
+
         }
+
+
     };
 
-    $scope.error1 = function(response){
+    $scope.error1 = function (response) {
         console.log('error', response);
         $scope.planning.error = true;
     };
-    $scope.error2 = function(response){
+    $scope.error2 = function (response) {
         console.log('error', resStops);
         $scope.planning.error = true;
     };
 
-    busService.getLinesFor().then($scope.lines,$scope.error1 );
-    busService.getStopsFor().then($scope.stops,$scope.error2);
+    busService.getLinesFor().then($scope.lines, $scope.error1);
+    busService.getStopsFor().then($scope.stops, $scope.error2);
 
 });
 
