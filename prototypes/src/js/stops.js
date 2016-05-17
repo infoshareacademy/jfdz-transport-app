a4ee9b4ae5af81d41e93b482095acf9398980f95
M.Wirkus <marcin@inkre.pl>
2016-05-17 13:30:36 +0200

    v. 0.0.3.7 - zakładka stops - localstorage

--- prototypes/src/js/stops.js
+++ prototypes/src/js/stops.js
@@ -12,28 +12,26 @@ mystops.controller('StopsController', function ($scope, $http, $filter, busServi
     $scope.stops = function (resStops) {
 
         $scope.stopsx = resStops.data;
-        $scope.mystops = [];
         $scope.$watch('stopsFilter', function (item) {
 
             console.log($scope.stopsFilter);
 
             if (item != undefined) {
 
-
                 //czas zamiana
 
-                var czas = new Date();
-                var time = '0710';
-                var h = time.slice(0, 2);
-                var m = time.slice(2, 4);
-
-                czas.setHours(h);
-                czas.setMinutes(m);
-
-                var hours = czas.getHours();
-                var minutes = czas.getMinutes();
-
-                console.log('godzina', hours + ':' + minutes);
+                // var czas = new Date();
+                // var time = '0710';
+                // var h = time.slice(0, 2);
+                // var m = time.slice(2, 4);
+                //
+                // czas.setHours(h);
+                // czas.setMinutes(m);
+                //
+                // var hours = czas.getHours();
+                // var minutes = czas.getMinutes();
+                //
+                // console.log('godzina', hours + ':' + minutes);
 
                 $scope.mstops = [];
                 var x = $scope.stopsx[item].bus;
@@ -41,18 +39,24 @@ mystops.controller('StopsController', function ($scope, $http, $filter, busServi
                     $scope.mstops.push(line);
                 });
 
-                
                 $scope.addstops = function () {
-
-                    $scope.mystops.push($scope.stopsFilter);
-                    $scope.showstops = $scope.mystops;
-
+                    $scope.mystops.push({
+                        id: $scope.stopsx[item].id,
+                        name: $scope.stopsx[item].name,
+                        bus: $scope.stopsx[item].bus
+                    });
+                    console.log($scope.mystops);
+                    localStorage.setItem('mystops', JSON.stringify($scope.mystops));
                 };
-
             }
-
-
         });
+        $scope.mystops = [];
+        angular.forEach($scope.mystops, function(item){
+            //$scope.xsline =  $scope.stopsx[item];
+        });
+        
+        $scope.savedstops = JSON.parse(localStorage.getItem('mystops'));
+        
     };
 
     $scope.error1 = function (response) {
