161a26af15ee83ca2ed617f7b32639093b623a26
M.Wirkus <marcin@inkre.pl>
2016-05-19 10:58:38 +0200

    v. 0.0.3.8 - zakładka lines - pierwsze próby

--- prototypes/src/js/lines.js
+++ prototypes/src/js/lines.js
@@ -1,1 +1,19 @@
+var lines = angular.module('lines', []);
 
+lines.controller('LinesController', function ($scope, busService) {
+
+    $scope.lines = function (response) {
+        $scope.lines.loading = false;
+        $scope.lines = response.data;
+
+        console.log($scope.lines)
+    };
+
+
+    busService.getLinesFor().then($scope.lines, $scope.error1);
+
+    $scope.error1 = function (response) {
+        $scope.planning.error = true;
+    };
+
+});
